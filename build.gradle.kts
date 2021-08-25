f2b11b29fb3c2d58fa309f230e1f91b7d54b43a9
Christoffer Berg <christofferberg79@gmail.com>
2021-08-25 16:16:21 +0200

    version updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.targets.js.webpack.KotlinWebpack
 
 plugins {
-    kotlin("multiplatform") version "1.5.21"
+    kotlin("multiplatform") version "1.5.30"
     application
 }
 
